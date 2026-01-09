13f5ace3d332f981b2956773dc9d7e642a577f26
AstraSpec <rizvan.faisal@gmail.com>
2026-01-09 11:38:10 +0000

    Structure: Adds create/existing filepath selection

--- spacetraveller/src/structure_editor/save_window.gd
+++ spacetraveller/src/structure_editor/save_window.gd
@@ -3,14 +3,19 @@ extends Window
 @export var structureEditor :Node2D
 @export var Editor :StructureEditor
 @export var StructureID :TextEdit
-@export var FPButton :Button
+
+@export var CreateButton :Button
+@export var ExistingButton :Button
+@export var CreateFP :Button
+@export var ExistingFP :Button
 
 const DIR_FILEPATH :String = "res://data/structures/"
 var currentPath :String = "res://data/structures/structures.json"
 
 func _ready() -> void:
 	structureEditor.open_save.connect(open)
-	FPButton.text = currentPath
+	CreateFP.text = DIR_FILEPATH
+	ExistingFP.text = currentPath
 
 func open() -> void:
 	visible = true
@@ -23,13 +28,27 @@ func _on_save_pressed() -> void:
 	DbAccess.save_structure(newID, newRLE, currentPath)
 	visible = false
 
+func _on_create_button_pressed() -> void:
+	CreateFP.visible = true
+	ExistingFP.visible = false
+
+func _on_existing_button_pressed() -> void:
+	CreateFP.visible = false
+	ExistingFP.visible = true
+
 func _on_close_pressed() -> void:
 	visible = false
 
 func _on_close_requested() -> void:
 	visible = false
 
-func _on_fp_button_pressed() -> void:
+func _on_create_fp_pressed() -> void:
+	show_dialog(DisplayServer.FILE_DIALOG_MODE_SAVE_FILE)
+
+func _on_existing_fp_pressed() -> void:
+	show_dialog(DisplayServer.FILE_DIALOG_MODE_OPEN_FILE)
+
+func show_dialog(dialogMode):
 	var globalPath = ProjectSettings.globalize_path(DIR_FILEPATH)
 	var filters = ["*.json ; JSON Files"]
 	
@@ -38,7 +57,7 @@ func _on_fp_button_pressed() -> void:
 		globalPath,
 		"",
 		false,
-		DisplayServer.FILE_DIALOG_MODE_OPEN_FILE,
+		dialogMode,
 		filters,
 		_on_file_selected)
 
@@ -50,4 +69,8 @@ func _on_file_selected(status: bool, selected_paths: PackedStringArray, _selecte
 			
 		var localPath = ProjectSettings.localize_path(path)
 		currentPath = localPath
-		FPButton.text = localPath
+		
+		if CreateFP.visible:
+			CreateFP.text = localPath
+		else:
+			ExistingFP.text = localPath
